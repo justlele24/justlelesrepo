b1bf60121309052c8e5966c4720656098b993a73
justlele24 <lele.barra2403@gmail.com>
2025-11-11 10:04:04 +0100

    first uploads

---
+++ motivation/motivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivationApp.swift
+//  motivation
+//
+//  Created by Raffaele Barra on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct motivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
